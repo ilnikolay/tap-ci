98950c03538299cfe640c112bc3645fa713aab6f
Nikolay <nikolayninov@infinitelambda.com>
2022-01-05 15:44:58 +0200

    Adding docstrings and README.md

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+'''Main app'''
+
 def add(n_1: int, n_2: int) -> int:
+    '''Return the sum of two numbers'''
     return n_1 + n_2
 
 
 def subtract(n_1: int, n_2: int) -> int:
+    '''Return the substract of two numbers'''
     return n_1 - n_2
 
 
